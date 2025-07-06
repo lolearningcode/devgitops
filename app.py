6b5256f3fa0908c449b98878298ab1525285993c
lolearningcode <lolearningcode@gmail.com>
2025-07-06 16:19:56 -0700

    updates to the app to hit the metrics path

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, path="/metrics")
 
 @app.route("/")
 def home():
     return "Welcome to the Cloud Engineer’s Playground! 😎🚀"
+
+# explicitly expose metrics
+@app.route("/metrics")
+def metrics_route():
+    return metrics.do_export()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
